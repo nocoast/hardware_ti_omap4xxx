CyanogenMod/android_hardware_ti_omap4xxx

[1/3] omap4xxx: Allow custom DOMX source instead of Google DOMX default
Part 1 of 3 patches: To allow omap4 devices to use custom "domx" source
via a new BoardConfig.mk item:
TI_CUSTOM_DOMX_PATH := device/<manufacturer>/<device-name>/domx

This setting provides for 3 changes during the build:
1. In hardware/ti/omap4xxx this settings stops standard Google domx
source from being built and changes the domx reference for tiutils.
2. In frameworks/base it changes the default openmax references for
frameworks/base/media/jni/mediaeditor/Android.mk to the new location
3. In frameworks/av changes the openmax references in 5 places, and adds
new includes in ACodec.cpp, CameraSource.cpp and OMXCodec.cpp

This commit for hardware/ti/omap4xxx is based on this cherry-pick from
omapzoom (with a more descriptive BoardConfig setting name):
http://www.omapzoom.org/?p=platform/hardware/ti/omap4xxx.git;a=commitdiff;h=040534d546dbcfcff16ff1afff15c01c2822dfcc

Change-Id: I32bf75073278639dc9e4442168c7b35295beb495
Signed-off-by: Hashcode <hashcode0f@gmail.com>

libtiutils/Android.mk
@@ -20,8 +20,16 @@ LOCAL_SHARED_LIBRARIES:= \
 
 LOCAL_C_INCLUDES += \
 	bionic/libc/include \
-	hardware/ti/omap4xxx/domx/omx_core/inc \
-	hardware/ti/omap4xxx/domx/mm_osal/inc
+
+ifneq ($(TI_CUSTOM_DOMX_PATH),)
+LOCAL_C_INCLUDES += \
+    $(DOMX_PATH)/omx_core/inc \
+    $(DOMX_PATH)/mm_osal/inc
+else
+LOCAL_C_INCLUDES += \
+    hardware/ti/omap4xxx/domx/omx_core/inc \
+    hardware/ti/omap4xxx/domx/mm_osal/inc
+endif
 	
 LOCAL_CFLAGS += -fno-short-enums 
 

nocoast/hardware_ti_omap4xxx

libtiutils/Android.mk
@@ -20,8 +20,16 @@ LOCAL_SHARED_LIBRARIES:= \
 
 LOCAL_C_INCLUDES += \
 	bionic/libc/include \
-	hardware/ti/omap4xxx/domx/omx_core/inc \
-	hardware/ti/omap4xxx/domx/mm_osal/inc
+
+ifneq ($(TI_CUSTOM_DOMX_PATH),)
+LOCAL_C_INCLUDES += \
+    $(TI_CUSTOM_DOMX_PATH)/omx_core/inc \
+    $(TI_CUSTOM_DOMX_PATH)/mm_osal/inc
+else
+LOCAL_C_INCLUDES += \
+    hardware/ti/omap4xxx/domx/omx_core/inc \
+    hardware/ti/omap4xxx/domx/mm_osal/inc
+endif
 	
 LOCAL_CFLAGS += -fno-short-enums 
 